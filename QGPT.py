74117510bd305d3c6e1f83effdd3b54018ba22f0
Jaskaran <jaskarann.singhh@gmail.com>
2023-03-26 00:50:28 +1100

    added a conversational mode that successively tracks a conversation in a continous experience

--- QGPT.py
+++ QGPT.py
@@ -10,36 +10,49 @@ def config_load():
     return config
 config = config_load()
 
-
-
 parser = argparse.ArgumentParser(description="Chat GPT CLI access")
 parser.add_argument('Prompt', type=str, nargs='+', help='Prompt Text')
 args = parser.parse_args()
 prompt = " ".join(args.Prompt)
+initialised_message = [{"role": "system", "content": config['quick_context']},
+                     {"role": "user", "content": prompt}]
 
-
-
-
-
-
-def get_completion(config, prompt):
+def get_completion(config, messages):
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {config['api-key']}",
     }
     body = {"model": config['model'], 
-        "messages": [{"role": "system", "content": config['quick_context']},
-                     {"role": "user", "content": prompt}],
-        "temperature": config['temperature']}
+            "temperature": config['temperature'],
+            "messages": messages
+            }
     
-    # response = requests.post(
-    #         "https://api.openai.com/v1/chat/completions", 
-    #         headers=headers, 
-    #         json=body
-    #     ).json()
-    
-    response = {'id': 'chatcmpl-6xxbLHmF9H4prvSmsYUuUc0wgfkCS', 'object': 'chat.completion', 'created': 1679748479, 'model': 'gpt-3.5-turbo-0301', 'usage': {'prompt_tokens': 12, 'completion_tokens': 21, 'total_tokens': 33}, 'choices': [{'message': {'role': 'assistant', 'content': "As an AI language model, I don't have a name, but you can call me OpenAI."}, 'finish_reason': 'stop', 'index': 0}]}
-    
-    return response
+    # response = {'id': 'chatcmpl-6xxbLHmF9H4prvSmsYUuUc0wgfkCS', 'object': 'chat.completion', 'created': 1679748479, 'model': 'gpt-3.5-turbo-0301', 'usage': {'prompt_tokens': 12, 'completion_tokens': 21, 'total_tokens': 33}, 'choices': [{'message': {'role': 'assistant', 'content': "As an AI language model, I don't have a name, but you can call me OpenAI."}, 'finish_reason': 'stop', 'index': 0}]}
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions", 
+        headers=headers, 
+        json=body
+        ).json()
+    # print(response["choices"][0]["message"])
+    return response["choices"][0]["message"]
 
-print(get_completion(config, prompt)["choices"][0]["message"]["content"])
+# print(get_completion(config, initialised_message)["content"])
+
+def conversation(config, messages):
+    prompt = input("Enter your prompt to start the conversation: ")
+    messages[1]['content'] = prompt
+    messages[0]['content'] = config["verbose_context"]
+    running = 1
+    while running == 1:
+        output = get_completion(config, messages)
+        messages.append(output)
+        print(messages)
+        print(output['content'])
+        prompt = input("> ")
+        if prompt == "/q":
+            break
+        messages.append({'role': 'user', 'content': prompt})
+    
+    
+    
+conversation(config, initialised_message)
